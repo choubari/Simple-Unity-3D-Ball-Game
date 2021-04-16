e1408c4a967bc8af6fe56c7b8cfc55adf8cdc05d
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2021-04-16 11:57:24 +0000

    first commit

---
+++ Assets/Scripts/ControleurJoueur.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControleurJoueur : MonoBehaviour
+{
+	public float vitesse;
+	private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();  
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector3 mouvement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(mouvement*vitesse);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+
+# Simple Unity 3D Ball Game
+
+A simple 3D Game to **discover Unity 3D**.
+
+
+## Week 1 :
+Manipulating the 3D scene, scripts and components :
+<p align="center">
+  <img  src="Screenshots/week1.gif" width="500px" >
